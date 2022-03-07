f9be17a6317d395a2426e8060f503d24038a62f8
Daniel Mendler <mail@daniel-mendler.de>
2022-03-07 11:16:05 +0100

    Version 0.2

--- org-modern.el
+++ org-modern.el
@@ -5,7 +5,7 @@
 ;; Author: Daniel Mendler <mail@daniel-mendler.de>
 ;; Created: 2022
 ;; License: GPL-3.0-or-later
-;; Version: 0.1
+;; Version: 0.2
 ;; Package-Requires: ((emacs "27.1"))
 ;; Homepage: https://github.com/minad/org-modern
 
