eace9688fdceda94d0429636ac9c1c7762c0fde0
JD Smith <93749+jdtsmith@users.noreply.github.com>
2023-03-09 20:21:08 -0500

    Minor doc

--- org-modern-indent.el
+++ org-modern-indent.el
@@ -27,7 +27,7 @@
 ;; org-modern-indent provides the block highlighting of org-modern,
 ;; when org-indent is enabled.
 ;;   
-;; Can be used with or without org-modern.
+;; Can be used with or without org-modern enabled.
 
 ;;; Code:
 (require 'compat)
