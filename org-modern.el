d812a192f040a9e7785a53f144de1800d52b9f0d
Daniel Mendler <mail@daniel-mendler.de>
2023-12-01 09:35:42 +0100

    Version 1.1

--- org-modern.el
+++ org-modern.el
@@ -5,8 +5,8 @@
 ;; Author: Daniel Mendler <mail@daniel-mendler.de>
 ;; Maintainer: Daniel Mendler <mail@daniel-mendler.de>
 ;; Created: 2022
-;; Version: 1.0
-;; Package-Requires: ((emacs "27.1") (compat "29.1.4.0"))
+;; Version: 1.1
+;; Package-Requires: ((emacs "27.1") (compat "29.1.4.4"))
 ;; Homepage: https://github.com/minad/org-modern
 ;; Keywords: outlines, hypermedia, wp
 
