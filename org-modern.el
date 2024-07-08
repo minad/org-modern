bf2cec740dcf41fe8b4cc1ecc537c4fc4c7e9403
Daniel Mendler <mail@daniel-mendler.de>
2024-07-08 23:57:18 +0200

    Use static-if

--- org-modern.el
+++ org-modern.el
@@ -681,7 +681,7 @@ whole buffer; otherwise, for the line at point."
        (list :color (face-attribute 'default :background nil t)
              :line-width
              ;; Emacs 28 supports different line horizontal and vertical line widths
-             (if (eval-when-compile (>= emacs-major-version 28))
+             (static-if (>= emacs-major-version 28)
                  (cons -1 (- border))
                (- border)))))))
 
