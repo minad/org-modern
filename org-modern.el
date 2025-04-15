fab253ab5e4e090d523463c228d1126c20daf2fa
Daniel Mendler <mail@daniel-mendler.de>
2025-04-15 19:11:15 +0200

    Update commentary

--- org-modern.el
+++ org-modern.el
@@ -29,7 +29,7 @@
 
 ;; This package adds some styling to your Org buffer, which gives it a
 ;; modern look.  Enable the styling by default with:
-;;   (add-hook 'org-mode-hook 'org-modern-mode)
+;;   (global-org-modern-mode)
 
 ;;; Code:
 
