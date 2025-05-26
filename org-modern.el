a58534475b4312b0920aa9d3824272470c8e3500
Daniel Mendler <mail@daniel-mendler.de>
2025-05-26 05:31:39 +0200

    Version 1.8

--- org-modern.el
+++ org-modern.el
@@ -5,7 +5,7 @@
 ;; Author: Daniel Mendler <mail@daniel-mendler.de>
 ;; Maintainer: Daniel Mendler <mail@daniel-mendler.de>
 ;; Created: 2022
-;; Version: 1.7
+;; Version: 1.8
 ;; Package-Requires: ((emacs "28.1") (org "9.5") (compat "30"))
 ;; URL: https://github.com/minad/org-modern
 ;; Keywords: outlines, hypermedia, text
