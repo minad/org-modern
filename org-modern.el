6c1c38d87fcaa794b64d847851fc54580c3e3b57
Daniel Mendler <mail@daniel-mendler.de>
2025-09-24 19:05:36 +0200

    Bump dependencies

--- org-modern.el
+++ org-modern.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Daniel Mendler <mail@daniel-mendler.de>
 ;; Created: 2022
 ;; Version: 1.9
-;; Package-Requires: ((emacs "28.1") (org "9.5") (compat "30"))
+;; Package-Requires: ((emacs "29.1") (org "9.6") (compat "30"))
 ;; URL: https://github.com/minad/org-modern
 ;; Keywords: outlines, hypermedia, text
 
